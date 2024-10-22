d47ee245a07fa28c99b228c673b68e3dbb18759f
surinnnnnn <didthfls2@naver.com>
2024-10-22 17:33:49 +0900

    update: 채팅, 입장 알림 기능 추가

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import {createServer} from 'http';
-import {Server} from 'socket.io';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-import cors from 'cors';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import cors from "cors";
 import * as path from "node:path";
+import { timeStamp } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,30 +14,47 @@ const app = express();
 app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+     cors: {
+          origin: "*",
+          methods: ["GET", "POST"],
+     },
 });
 
-io.on('connection', (socket) => {
-    console.log('사용자가 연결되었습니다', socket.id);
+io.on("connection", (socket) => {
+     console.log("사용자가 연결되었습니다", socket.id);
+     socket.broadcast.emit("SEND_MESSAGE", {
+          socketId: socket.id + Math.random(),
+          content: "새로운 사용자가 입장했습니다.",
+          sender: socket.id,
+          timeStamp: new Date(),
+     });
+     socket.on("SEND_MESSAGE", (msg) => {
+          //메세지 객체 처리
 
-    socket.on('SEND_MESSAGE', (msg) => {
-        console.log(msg);
-    });
+          const msgObj = {
+               socketId: socket.id + Math.random(),
+               content: msg,
+               sender: socket.id,
+               timeStamp: new Date(),
+          };
 
-    socket.on('disconnect', () => {
-        console.log('사용자가 연결을 끊었습니다');
-    });
+          console.log(msgObj);
+
+          //브로드 캐스트
+          io.emit("SEND_MESSAGE", msgObj);
+     });
+
+     socket.on("disconnect", () => {
+          console.log("사용자가 연결을 끊었습니다");
+     });
 });
 
-app.use(express.static(path.join(path.resolve(), 'public')));
-app.get('/*', (req,res) =>{
-    res.sendFile(path.join(path.resolve() , 'public', 'index.html'));
+app.use(express.static(path.join(path.resolve(), "public")));
+app.get("/*", (req, res) => {
+     res.sendFile(path.join(path.resolve(), "public", "index.html"));
 });
 
 const PORT = 4000;
 httpServer.listen(PORT, () => {
-    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다`);
+     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다`);
 });
